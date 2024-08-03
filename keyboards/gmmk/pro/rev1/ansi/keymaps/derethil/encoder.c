3c8e959a448a7f716829228c26b48c97eeab3e96
Jaren Glenn <jarenglenn@gmail.com>
2024-08-02 22:27:09 -0400

    docs: add qmk.nvim generated representation comments

---
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/derethil/encoder.c
@@ -0,0 +1,4 @@
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {[0] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)}, [1] = {ENCODER_CCW_CW(KC_TRNS, KC_TRNS)}};
+#endif
